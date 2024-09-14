9011cd84acea953e84eb89d8c28d361a5a8c57ea
Alejandro Cabrera <alejandro.cabrera.t@gmail.com>
2024-09-14 09:28:52 -0500

    feat: :art: prueba

---
+++ Node-JonasS/app.js
@@ -0,0 +1,13 @@
+//
+/* Deseo Crear una aplicacion web cuyo frontend este desarrollado con react y su backend con node.js haciendo uso del framework express.js.
+Esta aplicación me debe ayudar a planificar tareas que realizara mi equipo de modeladores en revit. Las tareas podran pasar con un drag and drop del estatus planificado , en proceso , realizado , archivado , eliminado , etc. Estos estatus podrian personalizarce su nombre , crear mas incluso. Tipo lo que tiene el app de trello.
+Las actividades seran de dos tipos , actividades Generales y actividades especificas.
+Las generales podran contener solo las especificas, que más adelante implementaremos mas features para cada tipo de actividades.
+
+La plataforma debe contemplar que debe ser multi pais , multi lenguaje.
+Los usuarios que se registren deben especificar su pais y acorde a ello el frontend debe mostrarse en cierto idioma , que el usuario tambien lo podria cambiar.
+
+Cada usuario puede crear diferentes proyectos , que al crearlo el , sera el administrador del proyecto.
+Para cada proyecto podre ir creando y planificando diferentes actividades.
+Debe implementar diferentes roles , como administrador , BIM Manager , Coordinador BIM , Supervisor BIM , Modelador BIM. Y poderle dar acceso a usuarios ya registrados en la plataforma. Ellos deberan aceptar la invitacion por correo electronico que la envio el administrador del proyecto. De esta manera dentro de su dashboard le saldra los proyectos propios y proyectos en los que colabora. */ 
+//
